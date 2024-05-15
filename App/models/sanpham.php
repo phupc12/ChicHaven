0c7e651291461cedba8b5c7b36a894d411150518
duythai170504 <thaicndps28634@fpt.edu.vn>
2024-05-15 15:45:18 +0700

    Update sanpham.php

--- App/models/sanpham.php
+++ App/models/sanpham.php
@@ -9,5 +9,27 @@
         $listsanpham=pdo_query($sql);
         return $listsanpham;
     }
+    function loadall_sanpham($kyw="", $iddm=0){
+        $sql ="select * from sanpham where 1";
+        if($kyw!=""){
+            $sql.="and name like '%".$kyw."%'";
+        }
+        if($iddm>0){
+            $sql.="and iddm ='".$iddm."'";
+        }
+        $sql.="order by id desc";
+        $listsanpham=pdo_query($sql);
+        return $listsanpham;
+    }
+    function load_sanpham($id){
+        $sql="select * from sanpham where id=".$id;
+        $sp=pdo_query_one($sql);
+        return $sp;
+    }
+    function load_sanphamdm($id, $category_id){
+        $sql="select * from sanpham where category_id =".$category_id." AND id <> ".$id;
+        $listsanpham=pdo_query($sql);
+        return $listsanpham;
+    }
   
 ?>
